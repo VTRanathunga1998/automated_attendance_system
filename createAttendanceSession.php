4aea40a199c2e3300ace8eb162509e48219d3b1e
Viraj Tharuka Ranathunga <virajtharuka.fb@gmail.com>
2023-02-22 15:38:07 +0530

    same session can create once

--- createAttendanceSession.php
+++ createAttendanceSession.php
@@ -29,55 +29,73 @@
 
     }
 
-    $sql = "SELECT faculty.facName,department.depName FROM faculty INNER JOIN department ON department.facID = faculty.facID WHERE department.depID = '$department'";
+    if(strtotime($timeStart) >= strtotime($timeEnd)){
+        echo '<script>if(confirm("Time slot does not match")) document.location = "adminTakeAttendance.php";</script>';
+        exit();
+    } else {
+        $sql = "SELECT faculty.facName,department.depName FROM faculty INNER JOIN department ON department.facID = faculty.facID WHERE department.depID = '$department'";
+
+        if(!mysqli_query($connect,$sql)){
+            die('Invalid query: ' . mysql_error());
+        } else {
+
+            $result = mysqli_query($connect,$sql);
+            $resultRows = mysqli_num_rows($result);
+
+            if($resultRows == 1){
+                while($row = mysqli_fetch_assoc($result)){  
+                $facultyn = $row['facName'];
+                $departmentn = $row['depName'];
+                
+                $sql = "SELECT sessionID FROM session WHERE faculty = '$facultyn' && department = '$departmentn' && activityType = '$activityType' && subject = '$subject' && semester = '$semester' && batch = '$batch' && timeStart = '$timeStart' && timeEnd = '$timeEnd' && date='$date'" ;
+
+                $result = mysqli_query($connect,$sql);
+                $resultRows = mysqli_num_rows($result);
+
+                if($resultRows == 1){
+                    echo '<script>if(confirm("Session has already created..")) document.location = "adminTakeAttendance.php";</script>';
+                    exit();
+                } else {
+                    $sql = "INSERT INTO session(faculty,department,activityType,subject,semester,batch,date,timeStart,timeEnd) VALUES('$facultyn','$departmentn' , '$activityType' , '$subject' , '$semester' , '$batch' , '$date' , '$timeStart' , '$timeEnd');";
+
+                    if(!mysqli_query($connect,$sql)){
+                            die('Invalid query: ' . mysql_error());
+                    } else {
+                            
+                            $sql = "SELECT sessionID FROM session WHERE faculty = '$facultyn' && department = '$departmentn' && activityType = '$activityType' && subject = '$subject' && semester = '$semester' && batch = '$batch' && timeStart = '$timeStart' && timeEnd = '$timeEnd' && date='$date'" ;
+
+                            $result = mysqli_query($connect,$sql);
+                            $resultRows = mysqli_num_rows($result);
+                    
+                            if($resultRows > 0){
+                                while($row = mysqli_fetch_assoc($result)){  
+                                    $_SESSION['sessionID'] = $row['sessionID'];
+                                    header("Location:scanQR.php?create=successful"); 
+                                    exit();
+                                }
+                            } else{
+                                echo '<script>if(confirm("Session create failed")) document.location = "adminProfile.php";</script>';
+                                exit();
+                            }
+                        
+                    }
+                }
+
+                }
+            }
+    
+        }
+    } 
 
     
-
-    if(!mysqli_query($connect,$sql)){
-        die('Invalid query: ' . mysql_error());
-    } else {
-
-        $result = mysqli_query($connect,$sql);
-        $resultRows = mysqli_num_rows($result);
-
-        if($resultRows == 1){
-            while($row = mysqli_fetch_assoc($result)){  
-               $facultyn = $row['facName'];
-               $departmentn = $row['depName'];
-               
-               $sql = "INSERT INTO session(faculty,department,activityType,subject,semester,batch,date,timeStart,timeEnd) VALUES('$facultyn','$departmentn' , '$activityType' , '$subject' , '$semester' , '$batch' , '$date' , '$timeStart' , '$timeEnd');";
-
-               if(!mysqli_query($connect,$sql)){
-                    die('Invalid query: ' . mysql_error());
-               } else {
-                    
-                    $sql = "SELECT sessionID FROM session WHERE faculty = '$facultyn' && department = '$departmentn' && activityType = '$activityType' && subject = '$subject' && semester = '$semester' && batch = '$batch' && timeStart = '$timeStart' && timeEnd = '$timeEnd' && date='$date'" ;
-
-                    $result = mysqli_query($connect,$sql);
-                    $resultRows = mysqli_num_rows($result);
-            
-                    if($resultRows > 0){
-                        while($row = mysqli_fetch_assoc($result)){  
-                            $_SESSION['sessionID'] = $row['sessionID'];
-                            header("Location:scanQR.php?create=successful"); 
-                            exit();
-                        }
-                    } else{
-                        header("Location:adminProfile.php?create=failed");
-                        exit();
-                    }
-                
-               }
-
-            }
-        }
-   
-    }
 
 ?>
 
 
 
+
+
+                
 
 
 
--- updateAttendanceExecute.php
+++ updateAttendanceExecute.php
@@ -21,63 +21,57 @@
         $timeEndMM = $_POST['timeEndMM'];
 
 
-
         $timeStart = $timeStartHH.':'.$timeStartMM.':00' ;
 
         $timeEnd = $timeEndHH.':'.$timeEndMM.':00' ;   
 
-        $sql = "SELECT faculty.facName,department.depName FROM faculty INNER JOIN department ON department.facID = faculty.facID WHERE department.depID = '$department'";
+
+            $sql = "SELECT faculty.facName,department.depName FROM faculty INNER JOIN department ON department.facID = faculty.facID WHERE department.depID = '$department'"; //covert value to text content
 
         
 
-        if(mysqli_query($connect,$sql)){
-            $result = mysqli_query($connect,$sql);
-            $resultRows = mysqli_num_rows($result);
+            if(mysqli_query($connect,$sql)){
+                $result = mysqli_query($connect,$sql);
+                $resultRows = mysqli_num_rows($result);
 
 
-            if($resultRows == 1){
-                while($row = mysqli_fetch_assoc($result)){  
+                if($resultRows == 1){
+                    while($row = mysqli_fetch_assoc($result)){  
 
-                    $faculty = $row['facName'];
-                    $department = $row['depName']; 
+                        $faculty = $row['facName'];
+                        $department = $row['depName']; 
 
-                    $sql = "SELECT sessionID FROM session WHERE faculty = '$faculty' AND batch = '$batch' AND activityType = '$activityType' AND subject = '$subject' AND semester = '$semester' AND department = '$department' AND date = '$date' AND timeStart = '$timeStart' AND timeEnd = '$timeEnd';" ;
+                        $sql = "SELECT sessionID FROM session WHERE faculty = '$faculty' AND batch = '$batch' AND activityType = '$activityType' AND subject = '$subject' AND semester = '$semester' AND department = '$department' AND date = '$date' AND timeStart = '$timeStart' AND timeEnd = '$timeEnd';" ;
 
-                    $result = mysqli_query($connect,$sql);
-                    $resultRows = mysqli_num_rows($result);
+                        $result = mysqli_query($connect,$sql);
+                        $resultRows = mysqli_num_rows($result);
 
-                    if($resultRows > 0){
-                        while($row=mysqli_fetch_assoc($result)){
-                            $sessionID = $row['sessionID'];
-                            $_SESSION["sessionID"] = $sessionID;
-                            header("Location:updateAttendanceExecuteLiveSearch.php");
+                        if($resultRows > 0){
+                            while($row=mysqli_fetch_assoc($result)){
+                                $sessionID = $row['sessionID'];
+                                $_SESSION["sessionID"] = $sessionID;
+                                header("Location:updateAttendanceExecuteLiveSearch.php");
+                                exit();
+                            }
+                        } else {
+                            echo '<script>if(confirm("Data does not exist")) document.location = "adminSettingHome.php";</script>';
                             exit();
                         }
-                    } else {
-                        // header("Location:adminSettingHome.php?data=does_not_exist");
-                        exit();
+
+                        
                     }
-
-                       
+                } else{
+                    echo '<script>if(confirm("Operation failed!..")) document.location = "adminProfile.php";</script>';
+                    exit();
                 }
-            } else{
-                // header("Location:adminProfile.php?create=failed");
-                exit();
+                
+            } else {
+
+                die('Invalid query: ' . mysql_error());
+                
             }
-            
-        } else {
+        } 
 
-            die('Invalid query: ' . mysql_error());
-            
-        }
-
-        
-
-        
-    }
-
-    
-   
 ?>
 
 
